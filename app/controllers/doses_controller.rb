5108e09b69bfa5cb6a76be055e0a5d7d25c1af35
moral nicolas <mrwallp@gmail.com>
2019-05-16 22:50:39 +0200

    dosesControllerDone

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,24 @@
 class DosesController < ApplicationController
+  def create
+    ingredients = Ingredient.where(id: [:ingredient_id])
+    cocktail = Cocktail.find(params[:cocktail_id])
+    ingredients.each do |ingredient|
+      @dose = Dose.new(ingredient: ingredient, cocktail: cocktail)
+      @dose.save
+    end
+  end
+
+  def new
+    @ingredient = Ingredient.all
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
+  end
+  redirect_to cocktails_path(@cocktails)
 end
