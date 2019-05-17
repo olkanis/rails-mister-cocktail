4e4a2e17a3efae2a1fd61677d3ba754d411768cd
moral nicolas <mrwallp@gmail.com>
2019-05-17 12:34:18 +0200

    doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,24 +1,31 @@
 class DosesController < ApplicationController
-  def create
-    ingredients = Ingredient.where(id: [:ingredient_id])
-    cocktail = Cocktail.find(params[:cocktail_id])
-    ingredients.each do |ingredient|
-      @dose = Dose.new(ingredient: ingredient, cocktail: cocktail)
-      @dose.save
-    end
-  end
-
   def new
     @ingredient = Ingredient.all
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose = Dose.new(doses_params)
+    if @dose.save
+      redirect_to cocktails_path(@cocktails)
+    else
+      render :new
+    end
+  end
+
   def destroy
     @dose = Dose.find(params[:id])
-    cocktail = @dose.cocktail
+    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(cocktail)
+    redirect_to cocktail_path(@cocktail)
   end
-  redirect_to cocktails_path(@cocktails)
+
+  private
+
+  def doses_params
+    params.require(:doses).permit(:description, :ingredient_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'json'
+require 'open-uri'
+
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient = open(url).read
+user = JSON.parse(ingredient)
+
+user['drinks'].each do |drink|
+  ingredient = Ingredient.create!(name: drink['strIngredient1'])
+  cocktail = Cocktail.create!(name: Faker::Beer.name)
+  Dose.create!(
+    description: ['1cl', '2cl', '3cl'].sample,
+    ingredient: ingredient, cocktail: cocktail
+  )
+end
